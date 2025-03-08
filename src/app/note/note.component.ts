0aedb8d0687f7e087ad97422a90c6ece68b66354
Daniel Dobosh <danieldobosh362@gmail.com>
2025-03-08 12:00:37 +0200

    Add popup

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,23 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-note',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './note.component.html',
-  styleUrl: './note.component.scss'
+  styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
-  showPopup = false;
-  isEditing = false;
-  notes: any[] = [];
+  isPopupOpen = false; // Состояние попапа
+  noteTitle = ''; // Данные заметки
+  noteContent = '';
 
-  openPopup(editing: boolean = false) {
-    this.isEditing = editing;
-    this.showPopup = true;
+  openPopup() {
+    this.isPopupOpen = true;
   }
 
   closePopup() {
-    this.showPopup = false;
+    this.isPopupOpen = false;
+  }
+
+  saveNote() {
+    console.log("Note saved:", this.noteTitle, this.noteContent);
+    this.closePopup(); 
   }
 }

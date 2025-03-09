de08119fe704ad5daaea6bc543cff685f5bc65b8
Daniel Dobosh <danieldobosh362@gmail.com>
2025-03-09 17:57:33 +0200

    Add Notes Details popup

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+interface Note {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-note',
   standalone: true,
@@ -10,10 +15,13 @@ import { RouterModule } from '@angular/router';
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
+
+
 export class NoteComponent {
-  isPopupOpen = false; // Состояние попапа
-  noteTitle = ''; // Данные заметки
+  isPopupOpen = false; 
+  noteTitle = ''; 
   noteContent = '';
+  notes: { title: string; content: string }[] = [];
 
   openPopup() {
     this.isPopupOpen = true;
@@ -25,6 +33,17 @@ export class NoteComponent {
 
   saveNote() {
     console.log("Note saved:", this.noteTitle, this.noteContent);
-    this.closePopup(); 
+    this.closePopup();
   }
+
+  selectedNote: Note | null = null;
+
+  openPopupNote(note: Note) {
+    this.selectedNote = note;
+  }
+
+  closePopupNote() {
+    this.selectedNote = null;
+  }
+
 }
